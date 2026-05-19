a69f4078de4e8cffa032e92100a70c40ba97af1a
akashRemarkableSolution <141607892+akashRemarkableSolution@users.noreply.github.com>
2026-05-19 17:55:39 +0530

    Initial commit with setup of plane and its movement pipes and laser animation

---
+++ Scenes/Tappy.cs
@@ -0,0 +1,70 @@
+using Godot;
+using System;
+
+public partial class Tappy : CharacterBody2D
+{
+	// Called when the node enters the scene tree for the first time.
+	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	const float JUMP_POWER = -350.0f;
+	[Export] AnimatedSprite2D sprite;
+
+	private bool _isJumped = false;
+	public override void _Ready()
+	{
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed("jump"))
+		{
+			_isJumped = true;
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		velocity.X = 100;
+
+		if (_isJumped)
+		{
+			velocity.Y = JUMP_POWER;
+			Velocity = velocity;
+			_isJumped = false;
+		}
+		else
+		{
+
+			ApplyGravity(delta);
+
+		}
+
+
+		MoveAndSlide();
+		if (IsOnFloor())
+		{
+			Die();
+		}
+
+	}
+
+    private void Die()
+    {
+		SetPhysicsProcess(false);
+		sprite.Stop();
+    }
+
+    private void ApplyGravity(double delta)
+	{
+		Vector2 downWardVelocity = Velocity;
+
+		downWardVelocity.Y += _gravity * (float)delta;
+
+		Velocity = downWardVelocity;
+
+	}
+
+	
+}
